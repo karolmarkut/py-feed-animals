551cc6cccf613aba9bc7b0f8000fb55e6209fbf8
Karol <karolmarkut11@gmail.com>
2026-02-07 14:49:37 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -35,11 +35,20 @@ class Cat(Animal):
 Wszystkie koty jedzą jednorazowo 3 punkty jedzenia i potrafią łapać myszy. Stwórz klasę Cat,
 która dziedziczy po klasie Animal. Jej metoda __init__ powinna przyjmować:
 name: imię kota.
-
 is_hungry: domyślnie True.
-
 Uwaga: Musisz wywołać metodę __init__ klasy nadrzędnej, ustawiając appetite na sztywno na wartość 3.
-
 Dodatkowa metoda:
-
 catch_mouse: powinna wypisać tekst: The hunt began!.
+class Dog(Animal):
+    def __init__(self, name: str, appetite: int, is_hungry: bool) -> None:
+        self.name = dog_name
+        self.appetite = 7
+        self.is_hungry = is_hungry
+    def bring_slippers(self):
+        print(f"The slippers delivered!")
+
+
+4. Funkcja feed_animals
+Zaimplementuj funkcję feed_animals, która przyjmuje listę obiektów zwierząt. Funkcja powinna:
+Nakarmić każde głodne zwierzę z listy (wywołując ich metody).
+Zwrócić sumę wszystkich punktów jedzenia, które były potrzebne do nakarmienia głodnych zwierząt.
